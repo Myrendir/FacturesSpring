cd58867a7abebb1a5a47112e9f1a775d6e9e0f2e
Myrendir <armand.deshais@gmail.com>
2022-01-12 16:03:36 +0100

    first commit

---
+++ src/main/java/com/app/FacturesSpring/FacturesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.app.FacturesSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/FacturesSpring/FacturesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.FacturesSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
